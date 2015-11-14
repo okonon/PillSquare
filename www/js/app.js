8a47c679960366f51bd27af9ac0e945211fea88a
davidwalto64 <dwalto64@gmail.com>
2015-11-14 15:47:50 -0500

    med screens

--- www/js/app.js
+++ www/js/app.js
@@ -114,6 +114,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
   // if none of the above states are matched, use this as the fallback
   //
-  $urlRouterProvider.otherwise('/app/medication-listing/1316024');
+  $urlRouterProvider.otherwise('/app/medication-listing/3860007');
 
 });
